51ac5e5a1a403e630aeb3532f83cf99fef7ef93f
longlonglong173 <71415818+longlonglong173@users.noreply.github.com>
2022-07-18 22:49:37 +0700

    base

---
+++ src/ui-controller.js
@@ -0,0 +1,74 @@
+import { entity } from './entity.js';
+
+export const ui_controller = (() => {
+    const _PHRASES = [
+        [' ', 5],
+        ['Bài tập nhóm 3D.', 10],
+        ['', 10],
+        ['', 10],
+        ['', 10],
+        ['', 10],
+        ['', 10],
+        ['', 10],
+        ['', 10],
+        ['', 10],
+        ['', 10],
+        ['', 10],
+        ['', 10],
+        ['', 10],
+        ['', 10],
+        ['', 10],
+    ];
+
+    class UIController extends entity.Component {
+        constructor(params) {
+            super();
+            this.params_ = params;
+            this.timeout_ = 0.0;
+            this.textArea_ = document.getElementById('chat-ui-text-area');
+            this.text_ = null;
+        }
+
+        AddText(txt) {
+            if (this.text_) {
+                this.text_ = null;
+            }
+
+            this.text_ = document.createElement('DIV');
+            this.text_.className = 'chat-text';
+            this.text_.innerText = txt;
+            this.text_.classList.toggle('fadeOut');
+
+            this.textArea_.appendChild(this.text_);
+
+            const dead = [];
+            for (let i = 0; i < this.textArea_.children.length; ++i) {
+                const s = window.getComputedStyle(this.textArea_.children[i]);
+                if (s.visibility == 'hidden') {
+                    dead.push(this.textArea_.children[i]);
+                }
+            }
+            for (let d of dead) {
+                this.textArea_.removeChild(d);
+            }
+        }
+
+        Update(timeElapsed) {
+            if (_PHRASES.length == 0) {
+                return;
+            }
+
+            this.timeout_ -= timeElapsed;
+            if (this.timeout_ < 0) {
+                const [phrase, timeout] = _PHRASES.shift();
+                this.timeout_ = timeout;
+                this.AddText(phrase);
+                return;
+            }
+        }
+    }
+
+    return {
+        UIController: UIController,
+    };
+})();
